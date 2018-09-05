8249f57c2fe2cf8497674b5a1de6184c4c2ce11d
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-09-05 16:38:31 +0300

    masters

--- src/main/java/finalproj/beautybar/service/ChooseDateService.java
+++ src/main/java/finalproj/beautybar/service/ChooseDateService.java
@@ -1,4 +1,31 @@
 package finalproj.beautybar.service;
 
+import finalproj.beautybar.dao.DAOFactory;
+import finalproj.beautybar.dao.IBookingDAO;
+import finalproj.beautybar.dao.ISceduleDAO;
+import finalproj.beautybar.entity.Booking;
+import finalproj.beautybar.entity.Scedule;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChooseDateService {
+
+    public Scedule getSceduleByMasterName(String name) throws Exception {
+        Scedule scedule = null;
+        List<Scedule> sceduleList = new ArrayList<>();
+        ISceduleDAO sceduleDAO = DAOFactory.getSceduleDAO();
+        sceduleDAO.findAll().stream().filter((p)-> p.getWorker().getName().equals(name)).forEach(sceduleList::add);
+        scedule = sceduleList.get(0);
+        return scedule;
+    }
+
+    public Scedule getFreeDatesByMasterName(String name) throws Exception {
+        Scedule scedule = null;
+        List<Booking> bookingList = new ArrayList<>();
+        IBookingDAO bookingDAO = DAOFactory.getBookingDAO();
+
+        bookingDAO.findAll().stream().filter((p)-> p.getWorkerService().getWorker().getName().equals(name)).forEach(bookingList::add);
+        return scedule;
+    }
 }
